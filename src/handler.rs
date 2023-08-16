52e9d624f6d67ab841518d4a9b53094f9110bc41
Bastian Büld <bastian.bueld@aoe.com>
2023-08-16 10:27:09 +0200

    refactor: remove .env

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{env, fs};
 use crate::http::{Request, StatusCode};
 use crate::http::method::Method;
 use crate::http::request::HttpParseError;
@@ -10,7 +10,11 @@ pub struct WebHandler {
 }
 
 impl WebHandler {
-    pub fn new(public_path: String) -> Self {
+    pub fn new() -> Self {
+        let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+        let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+        println!("public_path: {}", public_path);
+
         Self {
             public_path
         }
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,11 @@ mod http;
 mod handler;
 mod server;
 
-use std::env;
 use server::Server;
 use crate::handler::WebHandler;
 
 fn main() {
     // server is a Struct
-    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
-    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
-    println!("public_path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebHandler::new(public_path));
+    server.run(WebHandler::new());
 }
